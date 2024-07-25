fa9cbd336670bb6c80c9fb60786b6e4646f72a53
aesop <abc091044466@gmail.com>
2024-07-26 02:02:01 +0800

    vit_base

---
+++ fine_tuning_vit_base.py
@@ -0,0 +1,204 @@
+
+
+# %%
+from datasets import load_dataset
+
+# load cifar10 (only small portion for demonstration purposes) 
+train_ds, test_ds = load_dataset('cifar100', split=['train[:]', 'test[:]'])
+# split up training into training + validation
+splits = train_ds.train_test_split(test_size=0.1)
+train_ds = splits['train']
+val_ds = splits['test']
+
+# %%
+train_ds
+
+# %%
+
+train_ds[0]['img']
+
+# %%
+id2label = {id:label for id, label in enumerate(train_ds.features['fine_label'].names)}
+label2id = {label:id for id, label in id2label.items()}
+label2id
+
+# %%
+id2label[train_ds[0]['fine_label']]
+
+# %%
+from transformers import ViTImageProcessor
+
+processor = ViTImageProcessor.from_pretrained("google/vit-base-patch16-224-in21k")
+image_mean = processor.image_mean
+image_std = processor.image_std
+size = processor.size["height"]
+
+# %%
+from torchvision.transforms import (CenterCrop, 
+                                    Compose, 
+                                    Normalize, 
+                                    RandomHorizontalFlip,
+                                    RandomResizedCrop, 
+                                    Resize, 
+                                    ToTensor)
+
+normalize = Normalize(mean=image_mean, std=image_std)
+_train_transforms = Compose(
+        [
+            RandomResizedCrop(size),
+            RandomHorizontalFlip(),
+            ToTensor(),
+            normalize,
+        ]
+    )
+
+_val_transforms = Compose(
+        [
+            Resize(size),
+            CenterCrop(size),
+            ToTensor(),
+            normalize,
+        ]
+    )
+
+def train_transforms(examples):
+    examples['pixel_values'] = [_train_transforms(image.convert("RGB")) for image in examples['img']]
+    return examples
+
+def val_transforms(examples):
+    examples['pixel_values'] = [_val_transforms(image.convert("RGB")) for image in examples['img']]
+    return examples
+
+# %%
+# Set the transforms
+train_ds.set_transform(train_transforms)
+val_ds.set_transform(val_transforms)
+test_ds.set_transform(val_transforms)
+
+# %%
+train_ds[:2]
+
+# %%
+from torch.utils.data import DataLoader
+import torch
+
+def collate_fn(examples):
+    pixel_values = torch.stack([example["pixel_values"] for example in examples])
+    labels = torch.tensor([example["fine_label"] for example in examples])
+    return {"pixel_values": pixel_values, "fine_labels": labels}
+
+train_batch_size = 128
+eval_batch_size = 128
+
+train_dataloader = DataLoader(train_ds, shuffle=True, collate_fn=collate_fn, batch_size=train_batch_size)
+val_dataloader = DataLoader(val_ds, collate_fn=collate_fn, batch_size=eval_batch_size)
+test_dataloader = DataLoader(test_ds, collate_fn=collate_fn, batch_size=eval_batch_size)
+
+# %%
+batch = next(iter(train_dataloader))
+for k,v in batch.items():
+  if isinstance(v, torch.Tensor):
+    print(k, v.shape)
+
+# %%
+assert batch['pixel_values'].shape == (train_batch_size, 3, 224, 224)
+assert batch['fine_labels'].shape == (train_batch_size,)
+
+# %%
+next(iter(val_dataloader))['pixel_values'].shape
+
+# %%
+import pytorch_lightning as pl
+from transformers import ViTForImageClassification, AdamW
+import torch.nn as nn
+
+class ViTLightningModule(pl.LightningModule):
+    def __init__(self, num_labels=100):
+        super(ViTLightningModule, self).__init__()
+        self.vit = ViTForImageClassification.from_pretrained('google/vit-base-patch16-224-in21k',
+                                                              num_labels=100,
+                                                              id2label=id2label,
+                                                              label2id=label2id)
+
+    def forward(self, pixel_values):
+        outputs = self.vit(pixel_values=pixel_values)
+        return outputs.logits
+        
+    def common_step(self, batch, batch_idx):
+        pixel_values = batch['pixel_values']
+        labels = batch['fine_labels']
+        logits = self(pixel_values)
+
+        criterion = nn.CrossEntropyLoss()
+        loss = criterion(logits, labels)
+        predictions = logits.argmax(-1)
+        correct = (predictions == labels).sum().item()
+        accuracy = correct/pixel_values.shape[0]
+
+        return loss, accuracy
+      
+    def training_step(self, batch, batch_idx):
+        loss, accuracy = self.common_step(batch, batch_idx)     
+        # logs metrics for each training_step,
+        # and the average across the epoch
+        self.log("training_loss", loss)
+        self.log("training_accuracy", accuracy)
+
+        return loss
+    
+    def validation_step(self, batch, batch_idx):
+        loss, accuracy = self.common_step(batch, batch_idx)     
+        self.log("validation_loss", loss, on_epoch=True)
+        self.log("validation_accuracy", accuracy, on_epoch=True)
+
+        return loss
+
+    def test_step(self, batch, batch_idx):
+        loss, accuracy = self.common_step(batch, batch_idx)
+        self.log("testing_loss",loss,on_epoch=True)     
+        self.log("testing_accuracy",accuracy,on_epoch=True)
+        return loss
+
+    def configure_optimizers(self):
+        # We could make the optimizer more fancy by adding a scheduler and specifying which parameters do
+        # not require weight_decay but just using AdamW out-of-the-box works fine
+        return AdamW(self.parameters(), lr=5e-5)
+
+    def train_dataloader(self):
+        return train_dataloader
+
+    def val_dataloader(self):
+        return val_dataloader
+
+    def test_dataloader(self):
+        return test_dataloader
+
+# %%
+# Start tensorboard.
+#%load_ext tensorboard
+#%tensorboard --logdir ~/CIM/lightning_logs/
+
+# %%
+from pytorch_lightning import Trainer
+from pytorch_lightning.callbacks import EarlyStopping
+
+# for early stopping, see https://pytorch-lightning.readthedocs.io/en/1.0.0/early_stopping.html?highlight=early%20stopping
+early_stop_callback = EarlyStopping(
+    monitor='val_loss',
+    patience=5,
+    strict=False,
+    verbose=False,
+    mode='min'
+)
+
+model = ViTLightningModule()
+trainer = Trainer(callbacks=[EarlyStopping(monitor='validation_loss')])
+trainer.fit(model)
+
+# %%
+trainer.test()
+
+# %%
+
+
+
